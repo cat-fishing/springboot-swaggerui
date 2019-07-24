73fbc65b760fc51146faeb2287baed007ba23ef9
jinbaipeng <cat-fishing@new.github.com>
2019-07-24 11:15:16 +0800

    解决前台参数无法发送到后台

--- src/main/java/com/scg/springbootswaggerui/controller/login/LoginController.java
+++ src/main/java/com/scg/springbootswaggerui/controller/login/LoginController.java
@@ -1,14 +1,13 @@
 package com.scg.springbootswaggerui.controller.login;
 
+import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -27,9 +26,10 @@ import java.util.Date;
 public class LoginController {
 
     @ApiOperation(value = "获取登录时间接口", notes = "获取登录时间接口")
-    @ApiImplicitParam(name = "user", value = "用户", required = true, dataType = "String",paramType="path")
+    @ApiImplicitParam(name = "user", value = "用户", required = true, dataType = "String",paramType="query")
     @RequestMapping(value = "/login.do", method = { RequestMethod.POST,RequestMethod.GET})
     public Object login(HttpServletRequest request){
+        log.info("打印所有参数："+JSON.toJSONString(request.getParameterMap()));
         log.info("登陆用户为："+request.getParameter("user"));
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
